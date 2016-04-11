3275318022ceeded3f805a14f7ef3b663f5ad29e
calper95 <calper95@hotmail.com>
2016-04-11 18:42:57 -0400

    MainBoundry class

---
+++ src/boundry/MainBoundry.java
@@ -0,0 +1,13 @@
+package boundry;
+
+import javax.swing.JFrame;
+
+public class MainBoundry {
+	public void render(){}
+	public void swap(){}
+	public static void main(String [] args)
+	{
+		JFrame frame = new JFrame();
+	
+	}
+}
