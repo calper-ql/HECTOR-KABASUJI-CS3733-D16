df5eb79b44187eae135a48232cf11fb8519aeb32
Zachary Armsby <zarmsby@gmail.com>
2016-04-13 13:17:53 -0400

    builder started

--- src/builder/Builder.java
+++ src/builder/Builder.java
@@ -1,9 +1,13 @@
 package builder;
 
+import boundry.WindowManager;
+import controllers.MainController;
+
 public class Builder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WindowManager wmg = new WindowManager(200, 200, 640, 535);
+		wmg.link(new MainController());
 
 	}
 
