1fba8bd42c8a45f6400bc0279e97e2e1540e99ac
rmwiesenberg <rmwiesenberg@wpi.edu>
2016-04-18 15:57:25 -0400

    Changed to using the getter
    Yay getters!

--- src/entities/ReleaseNumberSet.java
+++ src/entities/ReleaseNumberSet.java
@@ -24,12 +24,12 @@ public class ReleaseNumberSet {
 		boolean con6 = false;
 		
 		for(int i = 0; i < numbers.size(); i++){
-			con1 |= (numbers.get(i).num == 1);
-			con2 |= (numbers.get(i).num == 2);
-			con3 |= (numbers.get(i).num == 3);
-			con4 |= (numbers.get(i).num == 4);
-			con5 |= (numbers.get(i).num == 5);
-			con6 |= (numbers.get(i).num == 6);
+			con1 |= (numbers.get(i).getNum() == 1);
+			con2 |= (numbers.get(i).getNum() == 2);
+			con3 |= (numbers.get(i).getNum() == 3);
+			con4 |= (numbers.get(i).getNum() == 4);
+			con5 |= (numbers.get(i).getNum() == 5);
+			con6 |= (numbers.get(i).getNum() == 6);
 		}
 		
 		return con1 & con2 & con3 & con4 & con5 & con6;
