3550c45b7eced31f7d3203081d0b241852ff847d
Zachary Armsby <zarmsby@gmail.com>
2016-04-18 15:48:17 -0400

    broke up level view to component views

--- src/boundary/LevelView.java
+++ src/boundary/LevelView.java
@@ -19,9 +19,13 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.border.LineBorder;
 
 public class LevelView {
-private JButton backButton;
+	private JButton backButton;
+	private BoardView boardView;
+	private BullPenView bullView;
 	
 	public LevelView() {
+		this.boardView = new BoardView();
+		this.bullView = new BullPenView();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -36,27 +40,6 @@ private JButton backButton;
 		backButton.setBounds(10, 5, 65, 50);
 		p.add(backButton);
 		
-		
-		
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(6, 100, 192, 384);
-		p.add(scrollPane);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setMaximumSize(new Dimension(100, 32767));
-		scrollPane.setViewportView(panel_1);
-		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(215, 100, 384, 384);
-		p.add(panel);
-		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Puzzle mode");
 		lblNewLabel.setForeground(Color.WHITE);
@@ -122,31 +105,8 @@ private JButton backButton;
 		label_1.setBounds(20, 54, 40, 40);
 		p.add(label_1);
 		
-		int X   = panel.getX();
-		int Y = panel.getY();
-		int widthP  = panel.getWidth();
-		int heightP = panel.getHeight();
-		
-		JPanel tiles[][] = new JPanel[12][12];
-		for(int i = 0;	i < 12; i++){
-			for(int k = 0; k < 12; k++){
-				
-				tiles[k][i] = new JPanel();
-				tiles[k][i].setVisible(true);
-				tiles[k][i].setBorder(new LineBorder(new Color(0, 0, 0)));
-				panel.add(tiles[k][i]);
-				int x =  ((widthP/12)*k);
-				int y = ((heightP/12)*i);
-				int w = (widthP/12);
-				int h = (heightP/12);
-				tiles[k][i].setBounds(x, y, w, h);
-				tiles[k][i].setBackground(new Color(250,255,255));
-				
-				if(((i==9) || (i == 11)) && (k == 7 || k == 9)){
-					tiles[k][i].setBackground(Color.darkGray);
-				}	
-			}
-		}
+		p.add(bullView.render());
+		p.add(boardView.render());
 		
 		return p;
 	}
