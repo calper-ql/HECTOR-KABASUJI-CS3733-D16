ffdd8ac583c990795aff22f26411de4496e35b42
calper95 <calper95@hotmail.com>
2016-04-18 15:08:56 -0400

    Implemented some of the entities (not fll implementation)

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -3,14 +3,14 @@ package entities;
 import java.util.ArrayList;
 
 public class Board {
-	ArrayList<Tile> tiles;
+	ArrayList<ArrayList<Tile>> tiles;
 	
-	public Board(ArrayList<Tile> tiles){
+	public Board(ArrayList<ArrayList<Tile>> tiles){
 		this.tiles = tiles;
 	}
 	
-	//not implemented at all
 	public Tile getTile(int col, int row){
-		return tiles.get(0);
+		return tiles.get(col).get(row);
 	}
+	
 }
---
+++ src/entities/EmptyBlock.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class EmptyBlock implements IBlock{
+	
+	EmptyBlock(){}
+
+	@Override
+	public void rotateBlock(IBlock block) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flipBlock(IBlock block) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
