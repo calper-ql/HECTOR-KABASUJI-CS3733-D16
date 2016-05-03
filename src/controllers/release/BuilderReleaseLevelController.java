f56238203d8a2db8bf0c8d91e12ce9a5f83e3b5c
calper95 <calper95@hotmail.com>
2016-05-03 18:52:13 -0400

    Release builder innitial

--- src/controllers/release/BuilderReleaseLevelController.java
+++ src/controllers/release/BuilderReleaseLevelController.java
@@ -1,23 +1,183 @@
 package controllers.release;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Stack;
+
 import javax.swing.JPanel;
 
+import boundary.JBlockPanel;
+import boundary.lightning.BuilderLightningLevelView;
+import boundary.puzzle.BuilderPuzzleLevelView;
+import boundary.release.BuilderReleaseLevelView;
+import controllers.BlockController;
+import controllers.BoardController;
 import controllers.BuilderLevelSelectController;
+import controllers.BullpenControler;
 import controllers.IController;
+import controllers.ILevelController;
 import controllers.MainController;
+import entities.EmptyBlock;
+import entities.Level;
 import entities.Model;
+import entities.PuzzleLevel;
 
-public class BuilderReleaseLevelController implements IController {
+public class BuilderReleaseLevelController implements IController, ILevelController {
 
-	public BuilderReleaseLevelController(MainController mc, BuilderLevelSelectController builderLevelSelectController,
-			Model model, int i) {
-		// TODO Auto-generated constructor stub
+	/* Things we need */
+	private MainController mainController;
+	private IController back;
+	private int levelNum;
+	private Model model;
+	private boolean bullpenBuilderModeIsEnabled;
+
+	/* Things we create */
+	private BuilderReleaseLevelView builderReleaseLevelView;
+	private BullpenControler bullpenController;
+	private BoardController boardController;
+	private BlockController blockController;
+	private JPanel renderPanel;
+	
+	/* for undo and redo */
+	private Stack<Level> levelStates;
+	private Stack<Level> redoStates;
+	
+	public BuilderReleaseLevelController(MainController mc, BuilderLevelSelectController back, Model model, int i) {
+		this.back = back;
+		this .levelNum = i;
+		this.mainController = mc;
+		this.model = model;
+		
+		init();
+	}
+	
+	private void init(){
+		builderReleaseLevelView = new BuilderReleaseLevelView(this);
+		blockController = new BlockController(new EmptyBlock(), this);
+		bullpenController = new BullpenControler(model.getLevel(levelNum).getBullpen(), blockController, this);
+		boardController = new BoardController(model.getLevel(levelNum), this);
+	}
+
+	/**
+	 * This function adds the button listeners and renders the bullpen and board
+	 * to the view.
+	 * 
+	 * @return panel
+	 */
+	@Override
+	public JPanel getRenderedView() {
+		renderPanel = builderReleaseLevelView.render();
+
+		// Back button
+		builderReleaseLevelView.getBackButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				backButtonClicked();
+			}
+		});
+
+		// Save button
+		builderReleaseLevelView.getSaveButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//saveButtonClicked();
+			}
+		});
+
+		// Reset button
+		builderReleaseLevelView.getResetButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//resetButtonClicked();
+			}
+
+		});
+
+		// Undo Button
+		builderReleaseLevelView.getUndoButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//undoButtonClicked();
+			}
+		});
+		
+		// Undo Button
+		builderReleaseLevelView.getRedoButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//redoButtonClicked();
+			}
+		});
+		
+		// Preview Button
+		builderReleaseLevelView.getPreviewButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//previewButtonClicked();
+			}
+
+		});
+
+		// Hint button
+		builderReleaseLevelView.getHintButton().addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == 1) {
+					boardController.disableBuilderMode();
+					boardController.enableHintSelection();
+				} else {
+					boardController.disableHintSelection();
+					boardController.enableBuilderMode();
+				}
+
+			}
+		});
+
+		// Both the bullpen and the board needs to be at the 0th layer.
+
+		// Warning!!!
+		// bullpenController must be enabled for builder !BEFORE! the rendering.
+		if (!bullpenBuilderModeIsEnabled) {
+			bullpenController.enableBuilderMode();
+			bullpenBuilderModeIsEnabled = true;
+		}
+		builderReleaseLevelView.getLayeredPane().add(bullpenController.render(), new Integer(0), 0);
+		bullpenController.disablePress();
+
+		// Warning!!!
+		// boardController must be enabled for builder !AFTER! the rendering.
+		builderReleaseLevelView.getLayeredPane().add(boardController.render(), new Integer(0), 0);
+		boardController.enableBuilderMode();
+
+		// return the renderPanel.
+		return renderPanel;
+	}
+	
+	// Reload the model before backing up.
+	private void backButtonClicked() {
+		model.reload();
+		// send the request to re-render to the higher controller
+		mainController.requestSwap(back);
 	}
 
 	@Override
-	public JPanel getRenderedView() {
+	public void piecePressed(JBlockPanel jBlockPanel) {
 		// TODO Auto-generated method stub
-		return null;
+		
+	}
+
+	@Override
+	public void pieceReleased(JBlockPanel jBlockPanel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void requestReRender() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stateUpdated() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
